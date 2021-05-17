1a59b9c29eca6e7cef541eb62b61693a51873522
Debabrata Deb <deb1999.me@gmail.com>
2021-05-18 01:07:12 +0530

    Debabrata comitted

--- OnlineE-GasBookingSystem/src/main/java/com/cg/service/SurrenderCylinderServiceImpl.java
+++ OnlineE-GasBookingSystem/src/main/java/com/cg/service/SurrenderCylinderServiceImpl.java
@@ -33,7 +33,6 @@ public class SurrenderCylinderServiceImpl implements ISurrenderCylinderService{
 		
 		Customer customer=null;
 		customer=customerDao.findByCustomerId(surrenderCylinderDto.getCustomerId());
-		
 		if(customer==null)
 			throw new CustomerNotFoundException("Customer not found");
 		customer.setConnectionStatus("inactive");
