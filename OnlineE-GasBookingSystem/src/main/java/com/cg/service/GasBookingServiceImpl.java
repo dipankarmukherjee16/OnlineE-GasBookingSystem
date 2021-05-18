84cb3771b9859d47ac97f11c4932e373c4897511
Moinak <Moinak@DESKTOP-KBJV93O>
2021-05-18 10:51:10 +0530

    Moinak committed

--- OnlineE-GasBookingSystem/src/main/java/com/cg/service/GasBookingServiceImpl.java
+++ OnlineE-GasBookingSystem/src/main/java/com/cg/service/GasBookingServiceImpl.java
@@ -1,26 +1,56 @@
 package com.cg.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cg.dao.ICustomerDao;
+import com.cg.dao.IGasBookingDao;
 import com.cg.dto.GasBookingDto;
+import com.cg.entity.Customer;
 import com.cg.entity.GasBooking;
 import com.cg.exception.CustomerNotFoundException;
 import com.cg.exception.GasBookingNotFoundException;
 
+@Transactional
 public class GasBookingServiceImpl implements IGasBookingService{
+	
+	@Autowired
+	private IGasBookingDao bookDao;
+	
+	@Autowired
+	private ICustomerDao custDao;
 
 	@Override
+	@Transactional
 	public Integer bookCylinder(GasBookingDto gasBookingdto) throws CustomerNotFoundException {
 		GasBooking book = new GasBooking();
 		book.setBookingDate(gasBookingdto.getBookingDate());
 		book.setStatus(gasBookingdto.getStatus());
 		book.setBill(gasBookingdto.getBill());
+		book.setDacNumber(gasBookingdto.getDacNumber());
+		book.setDispatchDate(gasBookingdto.getDispatchDate());
+		book.setCylinderId(gasBookingdto.getCylinderId());
 		
-		return null;
+		Optional<Customer> opcust = custDao.findById(gasBookingdto.getCustomerId());
+		if(!opcust.isPresent())
+			throw new CustomerNotFoundException("No Customer Found");
+		Customer cust = opcust.get();
+		book.setCustomer(cust);
+		GasBooking persistedBook = bookDao.save(book);
+		return persistedBook.getGasBookingId();
 	}
 
 	@Override
+	@Transactional
 	public boolean cancelGasBooking(int bookingId) throws GasBookingNotFoundException {
-		// TODO Auto-generated method stub
-		return false;
+		Optional<GasBooking> opbook = bookDao.findById(bookingId);
+		if(!opbook.isPresent())
+			throw new GasBookingNotFoundException("Gas Booking not found");
+		GasBooking book = opbook.get();
+		bookDao.delete(book);
+		return true;
 	}
 
 	@Override
