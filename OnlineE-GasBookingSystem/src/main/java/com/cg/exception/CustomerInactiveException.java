e27584e03d0600d84cbd5b2cb151d2c5cbb81eda
Dipankar Mukherjee <dipankarmukherjee10@gmail.com>
2021-06-21 13:15:52 +0530

    dipankar created

---
+++ OnlineE-GasBookingSystem/src/main/java/com/cg/exception/CustomerInactiveException.java
@@ -0,0 +1,16 @@
+package com.cg.exception;
+
+public class CustomerInactiveException extends Exception{
+
+	public CustomerInactiveException() {
+		super();
+		
+	}
+
+	public CustomerInactiveException(String message) {
+		super(message);
+		
+	}
+	
+
+}
